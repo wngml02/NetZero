04bf2b5dc0663923587b4da8030032b7c1259961
wngml <wngml021211@gmail.com>
2022-11-10 23:18:12 +0900

    css setting

--- netsite/netsite/settings.py
+++ netsite/netsite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
+    'netapp',
 ]
 
 MIDDLEWARE = [
@@ -54,9 +56,7 @@ ROOT_URLCONF = 'netsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-          TEMPLATE_DIR, #템플릿 경로 추가
-        ],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
