e5d9ebaee3915f05989a6457dd6c03326913e31e
Chaeyeon <codus1114@gmail.com>
2022-11-14 02:27:06 +0900

    view

--- netsite/posttwoapp/views.py
+++ netsite/posttwoapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def posttwo(request):
   return render(request, 'posttwoapp/post2.html')
+
+def my_view(request):
+  if request.method == 'POST':
+    eat1 = request.POST.getlist('eat1')
+    print(eat1)
+  return render(request, 'post2.html')
