c03da2ad1ddbdb80f2e400a16391f16d11fecd11
Chaeyeon <codus1114@gmail.com>
2022-11-15 00:50:35 +0900

    account

--- netsite/users/views.py
+++ netsite/users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from django.contrib import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def login(request):
@@ -9,3 +13,16 @@ def login(request):
 @csrf_exempt
 def search(request):
   return HttpResponse('success')
+
+def signup(request):
+    if request.method == 'POST':
+      if request.POST['password1'] == request.POST['password2']:
+        user = User.objects.create_user(
+          username = request.POST['username'],
+          password = request.POST['password1'],
+          email = request.POST['email'],
+        )
+        auth.login(request, user)
+        return redirect('/')
+      return render(request, 'signup.html')
+    return render(request, 'signup.html')
