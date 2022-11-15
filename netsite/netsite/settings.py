6abddd3a2eae9081ef8374b8d23ad39a344116bb
Chaeyeon <codus1114@gmail.com>
2022-11-16 04:48:40 +0900

    fighting

--- netsite/netsite/settings.py
+++ netsite/netsite/settings.py
@@ -137,3 +137,7 @@ STATICFILES_DIRS = (
 )
 LOGIN_REDIRECT_URL = "/posttwoapp"
 LOGOUT_REDIRECT_URL = "/posttwoapp"
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+)
