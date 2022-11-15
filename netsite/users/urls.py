a7bf1fc5f3bc7dca603767f5bc687f71031a1dac
wngml <wngml021211@gmail.com>
2022-11-16 01:56:46 +0900

    signup

--- netsite/users/urls.py
+++ netsite/users/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 from django.urls import path
-from .views import *
+from views import *
+import users.views
 
 urlpatterns = [
     path('users/', include('users.urls')),
@@ -10,8 +11,8 @@ urlpatterns = [
 
 app_name = "users"
 urlpatterns = [
-  path('login/', auth_views.LoginView.as_view(template_name='/users/login.html'), name="login"),
+  path('login/', users.views.login, name="login"),
   path("", views.login), 
-  path('logout/',auth_views.LogoutView.as_view(next_page="/posttwoapp/"),name='logout'),
-  path('signup/', views.signup, name='signup'),
+  path('logout/', users.views.logout, name='logout'),
+  path('signup/', users.views.signup, name='signup'),
 ]
--- netsite/users/views.py
+++ netsite/users/views.py
@@ -9,6 +9,15 @@ from users.forms import UserForm
 
 # Create your views here.
 def login(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth.authenticate(request, username=username, password=password)
+    if user is not None:
+      auth.login(request, user)
+      return redirect('/')
+    else:
+      return render(request, 'users/login.html', {'error': 'username or password is incorrect'})
   return render(request, 'users/login.html')
 
 @csrf_exempt
@@ -17,13 +26,14 @@ def search(request):
 
 def signup(request):
     if request.method == 'POST':
-      form = UserForm(request.POST)
-      if form.is_valid():
-        form.save()
-        username= form.cleaned_data.get['username']
-        raw_password = form.cleaned_data.get('password1')
-        user = authenticate(username=username,password=raw_password)
-        return redirect('posttwo')
-    else:
-        form = UserForm()
-    return render(request, 'users/signup.html', {'form':form})
+      if request.POST['password'] == request.POST['confirm']:
+        user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+        auth.login(request, user)
+        return redirect('/')
+    return render(request, 'users/signup.html')
+
+def logout(request):
+  if request.method == 'POST':
+    auth.logout(request)
+    return redirect('/')
+  return render(request,'users/login.html')
