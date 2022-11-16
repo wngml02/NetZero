8d05dd7840af9d48897e17b2acb11368eca2aa54
OJO <wngml021211@gmail.com>
2022-11-16 17:48:21 +0900

    유레카!!!!!!!!!!!

--- netsite/foliumapp/views.py
+++ netsite/foliumapp/views.py
@@ -14,25 +14,26 @@ from pandas.io.json import json_normalize
 from folium.plugins import HeatMap
   
 def show_map():
+  csvFile = 'foliumapp/Si_do.csv'
+  SiDodf = pd.read_csv(csvFile, encoding='utf-8')
+  jsonFile = 'foliumapp/Sidojson.json'
+  Sido_geo = json.load(open(jsonFile, encoding='utf-8'))
   
-        state_geo = "foliumapp/skorea-provinces-2018-topo-simple.json"
-        geo_data = json.load(open(state_geo, encoding='utf-8'))
-
-        df = pd.read_csv('foliumapp/Si_Do.csv', encoding='utf-8')
-        m = folium.Map(location=[36.45, 127.42], zoom_start=5)
-        m.choropleth(
-            geo_data = df,
-            name='choropleth',
-            data=df,
-            columns=['State','Unemployment'],
-            key_on = 'feature.properties.name_eng',
-            fill_color = 'YlGn',
-            fill_opacity=0.7,
-            legent_name='Rate'
-        )
-        folium.LayerControl().add_to(m)
-        m.save('./foliummm.html')
+  m=folium.Map(location=[36.45,127.42],title="OpenStreetMap",zoom_start=8)
+  folium.Choropleth(
+    geo_data=Sido_geo,
+    name="carbonmap",
+    data=SiDodf,
+    columns=['Sido','carbon'],
+    key_on='feature.properties.CTP_ENG_NM',
+    fill_color='PuRd',
+    fill_opacity=0.7,
+    legend_name='carbonmap').add_to(m)
+  folium.LayerControl().add_to(m)
+  
+  m.save('foliumapp/map.html')
+  webbrowser.open('foliumapp/map.html')
 
 def foliumm(request):
   show_map()
-  return render(request, '../templates/foliummm.html')
+  return render(request, 'foliumapp/map.html')
